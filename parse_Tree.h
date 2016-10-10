4c3ce2c8ecb482a73bfbf44c5ff91709cd54ec14
Luba Le Xuan <lubavable@mail.ru>
2016-10-10 14:54:59 -0400

    new parse tree classes

---
+++ parse_Tree.h
@@ -0,0 +1,11 @@
+/*
+Your parser should read successive expressions from standard input, construct
+a parse tree for the expression if it is well-formed, and then print that
+parse tree to standard output (see description below).
+ If the input is not well-formed, your parser should print an appropriate message
+and resume processing at the next line of input (i.e., skip to the next
+newline).
+ You may assume that expressions are on a single line of input if you want, or
+deal with multi-line input.
+ Your program should exit on end-of-file on standard input.
+*/
